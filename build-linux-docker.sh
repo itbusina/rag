f5c470bf4a59d769fe1a429bbd9bf6970d49990a
Kiryl Bahdanovich <work.kiryl.bahdanovich@gmail.com>
2025-12-27 16:37:48 +0100

    remove azure container registry

--- build-linux-docker.sh
+++ build-linux-docker.sh
@@ -1,3 +1,2 @@
 docker buildx build --platform linux/amd64 -t rag:amd64 -f src/api/Dockerfile .
 docker tag rag:amd64 itbusina/rag:amd64             # Docker Hub
-docker tag rag:amd64 itbusina.azurecr.io/rag:amd64  # Azure Container Registry
